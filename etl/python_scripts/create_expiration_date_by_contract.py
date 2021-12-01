eff2d13fe6a53d35fbb20d357c5c44b4c815b68a
Sam Cooper <cooper437@gmail.com>
2021-12-01 12:55:45 -0500

    Completed the create_expiration_date_by_contract script

--- etl/python_scripts/create_expiration_date_by_contract.py
+++ etl/python_scripts/create_expiration_date_by_contract.py
@@ -1,11 +1,21 @@
+'''
+Transform our futures contracts dataset from First Rate Data into a csv table
+that contains each contract and its respective expiration date. Write the csv to the
+filesystem
+'''
 import os
 import pandas as pd
+import time
 from tqdm import trange
 
-CONTRACTS_PREFIX_MATCHER = 'LEG'  # Limit to cattle futures for now
+CONTRACTS_PREFIX_MATCHER = ''  # Option limit if desired
 CURRENT_DIR = os.path.dirname(__file__)
 RAW_DATA_DIR = os.path.join(
     CURRENT_DIR, '../../data/raw/firstratedata_futures')
+PROCESSED_DATA_DIR = os.path.join(
+    CURRENT_DIR, '../../data/processed/futures_contracts')
+TARGET_FILENAME = 'expiration_date_by_contract.csv'
+TARGET_FILE_DEST = os.path.join(PROCESSED_DATA_DIR, TARGET_FILENAME)
 
 
 def convert_csv_to_df(filename):
@@ -42,8 +52,12 @@ def csv_files_to_analyze(data_dir, filename_prefix_matcher):
     return csv_files
 
 
+target_file_exists = os.path.exists(TARGET_FILE_DEST)
+if target_file_exists:
+    print('The target file already exists and will be overwritten. Abort now to cancel.')
+    time.sleep(5)
 csv_files = csv_files_to_analyze(
     data_dir=RAW_DATA_DIR, filename_prefix_matcher=CONTRACTS_PREFIX_MATCHER)
-print(f"Analyzing the following files: {csv_files}")
+print(f"Analyzing {len(csv_files)} the files")
 target_df = build_target_df(csv_files)
-print(target_df)
+target_df.to_csv(TARGET_FILE_DEST, index=False)
