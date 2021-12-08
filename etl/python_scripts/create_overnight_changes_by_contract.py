598f491c9b28a7ec60245aa700f87cc326119fba
Sam Cooper <cooper437@gmail.com>
2021-12-08 10:24:15 -0500

    WIP

--- etl/python_scripts/create_overnight_changes_by_contract.py
+++ etl/python_scripts/create_overnight_changes_by_contract.py
@@ -5,9 +5,9 @@ from pandas.core.frame import DataFrame
 from tqdm import trange
 from datetime import datetime, timedelta
 '''
-Analyze the raw contract data combined with our encriched contract open data.
+Analyze the raw contract data.
 Build out a new dataset that shows the overnight changes from one trading day to the next
-There are two strategies that can be used here:
+There are two strategies that can be used here leading to different outputs:
 OPEN_CLOSE_TRUE_OPEN:
     Open: use the open price IF there is a bar on the open; if no bar, exclude the overnight change calc for ALL close types for that day
     Close: use the close bar IF that close bar exists; if no bar, exclude the overnight change calc for ONLY that close type for that day
@@ -16,9 +16,15 @@ OPEN_CLOSE_SLIDING_OPEN:
     Close: use the close bar IF that close bar exists; for 11:59 and 12:04 CT bars, if there is no bar available for those time, use the last bar of that day
 '''
 
-OPEN_CLOSE_TRUE_OPEN = 'open_close_true_open'
-OPEN_CLOSE_SLIDING_OPEN = 'open_close_sliding_open'
-OPEN_CLOSE_BAR_ANALYSIS_STRATEGY = OPEN_CLOSE_SLIDING_OPEN
+
+class Open_Close_Bar_Strategy_Enum(enum.Enum):
+    true_open = 'true open and close'
+    sliding_open = 'sliding open and close'
+
+
+# OPEN_CLOSE_TRUE_OPEN = 'open_close_true_open'
+# OPEN_CLOSE_SLIDING_OPEN = 'open_close_sliding_open'
+OPEN_CLOSE_BAR_ANALYSIS_STRATEGY = Open_Close_Bar_Strategy_Enum.sliding_open
 CONTRACTS_PREFIX_MATCHER = 'LE'  # Optional limit if desired
 CURRENT_DIR = os.path.dirname(__file__)
 RAW_DATA_DIR = os.path.join(
@@ -27,9 +33,6 @@ PROCESSED_DATA_DIR = os.path.join(
     CURRENT_DIR, '../../data/processed/futures_contracts')
 UNIQUE_TRADING_DAYS_LE_CONTRACTS_FILE_PATH = os.path.join(
     PROCESSED_DATA_DIR, 'unique_trading_days_le_contracts.csv'
-)
-ENRICHED_CONTRACT_OPEN_FILE_PATH = os.path.join(
-    PROCESSED_DATA_DIR, 'contract_open_enriched_with_flat_open.csv'
 )
 TARGET_FILENAME = 'overnight_changes_by_contract.csv'
 TARGET_FILE_DEST = os.path.join(PROCESSED_DATA_DIR, TARGET_FILENAME)
@@ -81,17 +84,6 @@ def get_sliding_open_bar_for_day(a_date: datetime.date, a_days_bars_df: pd.DataF
         return None
     first_bar_of_day_within_open_window = days_within_open_window_df.iloc[0]
     return first_bar_of_day_within_open_window
-
-
-def convert_enriched_contract_open_csv_to_df(filename):
-    '''
-    Convert the previously enriched contract open data to a dataframe
-    '''
-    csv_as_df = pd.read_csv(
-        filename,
-        parse_dates=['DateTime'], usecols=['Symbol', 'DateTime', 'Open', 'High', 'Low', 'Close', 'Open Minutes Offset']
-    )
-    return csv_as_df
 
 
 def convert_contract_csv_to_df(filename):
@@ -160,14 +152,11 @@ csv_files = csv_files_to_analyze(
     data_dir=RAW_DATA_DIR, filename_prefix_matcher=CONTRACTS_PREFIX_MATCHER)
 unique_trading_days_all_le_contracts = convert_unique_trading_days_series(
     UNIQUE_TRADING_DAYS_LE_CONTRACTS_FILE_PATH)
-enriched_contract_open_df = convert_enriched_contract_open_csv_to_df(
-    ENRICHED_CONTRACT_OPEN_FILE_PATH)
-print(f"Analyzing {len(csv_files)} files")
-for contract_to_process in trange(len(csv_files[0:1])):
+print(
+    f"Analyzing {len(csv_files)} files using a {OPEN_CLOSE_BAR_ANALYSIS_STRATEGY.value} strategy")
+for contract_to_process in trange(len(csv_files)):
     file = csv_files[contract_to_process]
     contract_symbol = file[0:-4]
-    enriched_contract_open_this_contract = enriched_contract_open_df[
-        enriched_contract_open_df['Symbol'] == contract_symbol]
     print(f"Analyzing data for {contract_symbol}")
     contract_df = convert_contract_csv_to_df(file)
     unique_dates_for_contract = sorted(
@@ -203,11 +192,12 @@ for contract_to_process in trange(len(csv_files[0:1])):
         prior_trading_day_last_bar = prior_trading_days_bars_df.iloc[-1]
         this_days_open_bar = get_true_open_bar_for_day(a_date=a_date)
         if this_days_open_bar is None:  # The true open bar is missing for this day
-            if OPEN_CLOSE_BAR_ANALYSIS_STRATEGY == OPEN_CLOSE_TRUE_OPEN:  # We are using a true open strategy
+            if OPEN_CLOSE_BAR_ANALYSIS_STRATEGY.name == Open_Close_Bar_Strategy_Enum.true_open.name:  # We are using a true open strategy
                 overnight_changes_df = overnight_changes_df.append(generate_empty_day_bar(
                     contract_symbol, a_date), ignore_index=True)
                 continue
-            elif OPEN_CLOSE_BAR_ANALYSIS_STRATEGY == OPEN_CLOSE_SLIDING_OPEN:
+            # We are using a sliding open strategy
+            elif OPEN_CLOSE_BAR_ANALYSIS_STRATEGY.name == Open_Close_Bar_Strategy_Enum.sliding_open.name:
                 this_days_open_bar = get_sliding_open_bar_for_day(
                     a_date=a_date, a_days_bars_df=a_days_bars_df)
                 if this_days_open_bar is None:  # There are no bars inside the open window for this day to use
