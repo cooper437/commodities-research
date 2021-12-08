3885cfc94c65356a5ba4a664e097b72ef82bdc4d
Sam Cooper <cooper437@gmail.com>
2021-12-08 09:05:07 -0500

    WIP

--- etl/python_scripts/create_overnight_changes_by_contract.py
+++ etl/python_scripts/create_overnight_changes_by_contract.py
@@ -4,15 +4,32 @@ import pandas as pd
 from pandas.core.frame import DataFrame
 from tqdm import trange
 from datetime import datetime
+'''
+Analyze the raw contract data combined with our encriched contract open data.
+Build out a new dataset that shows the overnight changes from one trading day to the next
+There are two strategies that can be used here:
+OPEN_CLOSE_TRUE_OPEN:
+    Open: use the open price IF there is a bar on the open; if no bar, exclude the overnight change calc for ALL close types for that day
+    Close: use the close bar IF that close bar exists; if no bar, exclude the overnight change calc for ONLY that close type for that day
+OPEN_CLOSE_SLIDING_OPEN:
+    Open: use the first available bar during the open timeframe (first 60 min after 8:30 CT); if no bar during open timeframe, exclude the overnight change calc for ALL close types for that day
+    Close: use the close bar IF that close bar exists; for 11:59 and 12:04 CT bars, if there is no bar available for those time, use the last bar of that day
+'''
 
+OPEN_CLOSE_TRUE_OPEN = 'open_close_true_open'
+OPEN_CLOSE_SLIDING_OPEN = 'open_close_sliding_open'
+OPEN_CLOSE_BAR_ANALYSIS_STRATEGY = OPEN_CLOSE_TRUE_OPEN
 CONTRACTS_PREFIX_MATCHER = 'LE'  # Optional limit if desired
 CURRENT_DIR = os.path.dirname(__file__)
 RAW_DATA_DIR = os.path.join(
     CURRENT_DIR, '../../data/raw/firstratedata_futures')
 PROCESSED_DATA_DIR = os.path.join(
     CURRENT_DIR, '../../data/processed/futures_contracts')
-UNIQUE_TRADING_DAYS_LE_CONTRACTS = os.path.join(
+UNIQUE_TRADING_DAYS_LE_CONTRACTS_FILE_PATH = os.path.join(
     PROCESSED_DATA_DIR, 'unique_trading_days_le_contracts.csv'
+)
+ENRICHED_CONTRACT_OPEN_FILE_PATH = os.path.join(
+    PROCESSED_DATA_DIR, 'contract_open_enriched_with_flat_open.csv'
 )
 TARGET_FILENAME = 'overnight_changes_by_contract.csv'
 TARGET_FILE_DEST = os.path.join(PROCESSED_DATA_DIR, TARGET_FILENAME)
@@ -30,7 +47,32 @@ def contract_open_time(trading_bar_datetime: datetime):
         return trading_bar_datetime.replace(hour=10, minute=5, second=0, microsecond=0)
 
 
+def get_true_open_bar_for_day(a_date: datetime.date):
+    # Convert the date to a datetime
+    as_datetime = datetime.combine(a_date, datetime.min.time())
+    this_days_true_open_time = contract_open_time(as_datetime)
+    this_days_true_open_bar = a_days_bars_df[a_days_bars_df['DateTime']
+                                             == this_days_true_open_time]
+    if this_days_true_open_bar.empty:  # The true open bar is missing for this date
+        return None
+    return this_days_true_open_bar
+
+
+def convert_enriched_contract_open_csv_to_df(filename):
+    '''
+    Convert the previously enriched contract open data to a dataframe
+    '''
+    csv_as_df = pd.read_csv(
+        filename,
+        parse_dates=['DateTime'], usecols=['Symbol', 'DateTime', 'Open', 'High', 'Low', 'Close', 'Open Minutes Offset']
+    )
+    return csv_as_df
+
+
 def convert_contract_csv_to_df(filename):
+    '''
+    Convert the raw contract csv data into a dataframe
+    '''
     csv_as_df = pd.read_csv(
         f"{RAW_DATA_DIR}/{filename}",
         parse_dates=['DateTime'], usecols=['DateTime', 'Open', 'High', 'Low', 'Close', 'Volume']
@@ -92,11 +134,15 @@ overnight_changes_df = pd.DataFrame(
 csv_files = csv_files_to_analyze(
     data_dir=RAW_DATA_DIR, filename_prefix_matcher=CONTRACTS_PREFIX_MATCHER)
 unique_trading_days_all_le_contracts = convert_unique_trading_days_series(
-    UNIQUE_TRADING_DAYS_LE_CONTRACTS)
+    UNIQUE_TRADING_DAYS_LE_CONTRACTS_FILE_PATH)
+enriched_contract_open_df = convert_enriched_contract_open_csv_to_df(
+    ENRICHED_CONTRACT_OPEN_FILE_PATH)
 print(f"Analyzing {len(csv_files)} files")
 for contract_to_process in trange(len(csv_files[0:1])):
     file = csv_files[contract_to_process]
     contract_symbol = file[0:-4]
+    enriched_contract_open_this_contract = enriched_contract_open_df[
+        enriched_contract_open_df['Symbol'] == contract_symbol]
     print(f"Analyzing data for {contract_symbol}")
     contract_df = convert_contract_csv_to_df(file)
     unique_dates_for_contract = sorted(
@@ -130,7 +176,12 @@ for contract_to_process in trange(len(csv_files[0:1])):
             a_days_bars_df=prior_trading_days_bars_df, time_to_get=prior_trading_day_thirteen_oh_four_datetime
         )
         prior_trading_day_last_bar = prior_trading_days_bars_df.iloc[-1]
-        this_days_open_bar = a_days_bars_df.iloc[0]
+        this_days_open_bar = get_true_open_bar_for_day(a_date=a_date)
+        if OPEN_CLOSE_BAR_ANALYSIS_STRATEGY == OPEN_CLOSE_TRUE_OPEN:  # We are using a true open strategy
+            if this_days_open_bar is None:  # The true open bar is missing for this day
+                overnight_changes_df = overnight_changes_df.append(generate_empty_day_bar(
+                    contract_symbol, a_date), ignore_index=True)
+                continue
         todays_open_price = this_days_open_bar['Open']
         twelve_fifty_nine_price_change = calculate_change_from_prior_day_bar(
             todays_open_price=todays_open_price, prior_day_bar=prior_trading_day_twelve_fifty_nine_bar)
