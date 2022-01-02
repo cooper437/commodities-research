b573f8157630c89c5902463fc09239d1ea52cce7
Sam Cooper <cooper437@gmail.com>
2022-01-02 11:05:49 -0500

    WIP

--- etl/python_scripts/create_cot_signals_table.py
+++ etl/python_scripts/create_cot_signals_table.py
@@ -4,6 +4,7 @@ import os
 import datetime
 import operator
 import pandas as pd
+from tqdm import trange
 
 CURRENT_DIR = os.path.dirname(__file__)
 COMMITMENT_OF_TRADERS_REPORTS_BASE_PATH = os.path.join(
@@ -34,6 +35,14 @@ def intraday_open_csv_to_df(filename) -> pd.DataFrame:
         ]
     )
     return csv_as_df
+
+
+def initialize_cot_analytics_table_df() -> pd.DataFrame:
+    '''A dataframe that contains the structure needed for the analytics table that is output by the script'''
+    columns = ['Report Name', 'Field Name', 'Open Type', 'Above/Below Median', 'Median Value', 'AFCO t=0',
+               'AFCO t+5', 'AFCO t+15', 'AFCO t+30', 'AFCO t+40', 'AFCO t+50', 'AFCO t+60']
+    initialized_df = pd.DataFrame(columns=columns)
+    return initialized_df
 
 
 def cot_csv_to_df(filename) -> pd.DataFrame:
@@ -125,12 +134,14 @@ def calculate_average_intraday_price_change_grouped_by_open_minutes_offset(
     return to_return_df
 
 
-def process_file(a_file: str, intraday_df: pd.DataFrame):
+def process_file(a_file: str, intraday_df: pd.DataFrame, open_type: str):
+    cot_analytics_table_df = initialize_cot_analytics_table_df()
     csv_as_df = cot_csv_to_df(
         os.path.join(COMMITMENT_OF_TRADERS_REPORTS_BASE_PATH, a_file))
     columns = csv_as_df.columns.values.tolist()
     reportable_columns = list(filter(is_nonreportable_column, columns))
-    for a_column in reportable_columns:
+    for i in trange(len(reportable_columns)):
+        a_column = reportable_columns[i]
         median_value_for_column = csv_as_df[a_column].median()
         cot_split_by_median = filter_and_split_cot_df_by_col_median_value(
             cot_df=csv_as_df, median_value=median_value_for_column, column_name=a_column)
@@ -141,17 +152,69 @@ def process_file(a_file: str, intraday_df: pd.DataFrame):
         )
         open_intraday_average_changes = calculate_average_intraday_price_change_grouped_by_open_minutes_offset(
             intraday_split_by_cot_df_median)
-        print(median_value_for_column)
+        # Capture above median stats
+        cot_analytics_table_df = cot_analytics_table_df.append({
+            'Report Name': a_file[:-4],
+            'Field Name': a_column,
+            'Above/Below Median': 'above',
+            'Open Type': open_type,
+            'Median Value': median_value_for_column,
+            'AFCO t=0': open_intraday_average_changes.iloc[0]['Avg Intraday Price Change When COT Field Above Median'],
+            'AFCO t+5': open_intraday_average_changes.iloc[4]['Avg Intraday Price Change When COT Field Above Median'],
+            'AFCO t+15': open_intraday_average_changes.iloc[14]['Avg Intraday Price Change When COT Field Above Median'],
+            'AFCO t+30': open_intraday_average_changes.iloc[29]['Avg Intraday Price Change When COT Field Above Median'],
+            'AFCO t+40': open_intraday_average_changes.iloc[39]['Avg Intraday Price Change When COT Field Above Median'],
+            'AFCO t+50': open_intraday_average_changes.iloc[49]['Avg Intraday Price Change When COT Field Above Median'],
+            'AFCO t+60': open_intraday_average_changes.iloc[59]['Avg Intraday Price Change When COT Field Above Median']
+        }, ignore_index=True)
+        # Now capture below median stats
+        cot_analytics_table_df = cot_analytics_table_df.append({
+            'Report Name': a_file[:-4],
+            'Field Name': a_column,
+            'Above/Below Median': 'below',
+            'Open Type': open_type,
+            'Median Value': median_value_for_column,
+            'AFCO t=0': open_intraday_average_changes.iloc[0]['Avg Intraday Price Change When COT Field Below Median'],
+            'AFCO t+5': open_intraday_average_changes.iloc[4]['Avg Intraday Price Change When COT Field Below Median'],
+            'AFCO t+15': open_intraday_average_changes.iloc[14]['Avg Intraday Price Change When COT Field Below Median'],
+            'AFCO t+30': open_intraday_average_changes.iloc[29]['Avg Intraday Price Change When COT Field Below Median'],
+            'AFCO t+40': open_intraday_average_changes.iloc[39]['Avg Intraday Price Change When COT Field Below Median'],
+            'AFCO t+50': open_intraday_average_changes.iloc[49]['Avg Intraday Price Change When COT Field Below Median'],
+            'AFCO t+60': open_intraday_average_changes.iloc[59]['Avg Intraday Price Change When COT Field Below Median']
+        }, ignore_index=True)
+    return cot_analytics_table_df
 
 
+final_df = initialize_cot_analytics_table_df()
 csv_files = list_reportable_files(COMMITMENT_OF_TRADERS_REPORTS_BASE_PATH)
 intraday_sliding_open_df = intraday_open_csv_to_df(
     CONTRACT_INTRADAY_SLIDING_OPEN_FILE_PATH)
 intraday_true_open_df = intraday_open_csv_to_df(
     CONTRACT_INTRADAY_TRUE_OPEN_FILE_PATH)
-# intraday_true_open_df['Date Of Preceding Tuesday'] = intraday_true_open_df['DateTime'].apply(
-#     date_of_preceding_tuesday)
+intraday_true_open_df['Date Of Preceding Tuesday'] = intraday_true_open_df['DateTime'].apply(
+    date_of_preceding_tuesday)
 intraday_sliding_open_df['Date Of Preceding Tuesday'] = intraday_sliding_open_df['DateTime'].apply(
     date_of_preceding_tuesday)
-results = [process_file(a_file, intraday_sliding_open_df)
-           for a_file in csv_files]
+
+sliding_open_results_by_cot_reports = [
+    process_file(
+        a_file=a_file,
+        intraday_df=intraday_sliding_open_df,
+        open_type='sliding_open'
+    )
+    for a_file in csv_files
+]
+for a_report in sliding_open_results_by_cot_reports:
+    final_df = pd.concat([final_df, a_report], ignore_index=True)
+
+true_open_results_by_cot_reports = [
+    process_file(
+        a_file=a_file,
+        intraday_df=intraday_true_open_df,
+        open_type='true_open'
+    )
+    for a_file in csv_files
+]
+for a_report in true_open_results_by_cot_reports:
+    final_df = pd.concat([final_df, a_report], ignore_index=True)
+print(final_df)
