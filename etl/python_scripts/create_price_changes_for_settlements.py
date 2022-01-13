90c3f05935df4d19b7a1c493b70efacbd20ce3d6
Sam Cooper <cooper437@gmail.com>
2022-01-13 16:34:37 -0500

    WIP

--- etl/python_scripts/create_price_changes_for_settlements.py
+++ etl/python_scripts/create_price_changes_for_settlements.py
@@ -28,9 +28,26 @@ TRUE_OPEN_TARGET_FILEPATH = os.path.join(
 SLIDING_OPEN_TARGET_FILEPATH = os.path.join(
     PROCESSED_DATA_DIR, 'overnight_changes_from_settlement_sliding_open.csv'
 )
+UNIQUE_TRADING_DAYS_LE_CONTRACTS_FILE_PATH = os.path.join(
+    PROCESSED_DATA_DIR, 'unique_trading_days_le_contracts.csv'
+)
 
 logging.basicConfig(
     format='%(asctime)s - %(levelname)s:%(message)s', level=logging.DEBUG)
+
+
+class Settlement_Comparison_Interval(enum.Enum):
+    '''
+    Represents the interval of time between the open price and the settlement price we are comparing it with
+    Overnight - Compare a days open price with the prior days settlement price
+    Weekly - Compare a days open price with a settlement price from 7 days ago.
+    Monthly - Compare a days open price with a settlement price from 4 weeks ago
+    Annualy - Compare a days open price with a settlement price from a year ago
+    '''
+    OVERNIGHT = 'overnight'
+    WEEKLY = 'weekly'
+    MONTHLY = 'monthly'
+    ANNUALY = 'annualy'
 
 
 def settlement_csv_files_to_analyze(data_dir):
@@ -40,6 +57,12 @@ def settlement_csv_files_to_analyze(data_dir):
         csv_files.append(file)
     csv_files.sort()
     return csv_files
+
+
+def convert_unique_trading_days(filepath) -> List[datetime.date]:
+    csv_as_df = pd.read_csv(filepath, parse_dates=[
+                            'DateTime'], usecols=['DateTime'])
+    return sorted(csv_as_df['DateTime'].dt.date)
 
 
 def intraday_open_to_df(filename) -> pd.DataFrame:
@@ -71,7 +94,7 @@ def contract_month_and_year_from_file_name(filename: str) -> Tuple[str]:
     return settlement_data_contract_month + settlement_data_contract_year[-2:]
 
 
-def calculate_overnight_change_from_settlement(a_days_open_bar, a_prior_days_settlement_bar):
+def calculate_change_between_open_and_prior_settlement(a_days_open_bar, a_prior_days_settlement_bar):
     # we are missing an open price or prior day settlement price
     if a_days_open_bar is None or a_prior_days_settlement_bar is None:
         return None
@@ -88,17 +111,49 @@ def get_first_bar_available_for_day(a_date: datetime.date, a_contracts_open_data
     return first_avail_bar
 
 
-def get_settlement_data_for_date(a_date: datetime.date, settlement_data_df: pd.DataFrame) -> pd.Series:
-    data_for_date = settlement_data_df[settlement_data_df['Date'].dt.date == a_date]
-    number_of_rows = len(data_for_date.index)
+def get_settlement_data_for_previous_trading_day(
+    a_date: datetime.date,
+    settlement_data_df: pd.DataFrame,
+    unique_trading_days: List[datetime.date]
+) -> pd.Series:
+    index_of_a_date = unique_trading_days.index(a_date)
+    try:
+        prior_trading_day_date = unique_trading_days[index_of_a_date - 1]
+    except ValueError:  # There is no prior trading day we're aware of
+        return None
+    data_for_previous_trading_day = settlement_data_df[settlement_data_df['Date'].dt.date ==
+                                                       prior_trading_day_date]
+    number_of_rows = len(data_for_previous_trading_day.index)
     if number_of_rows == 0:
         return None
-    if number_of_rows > 1:
+    if number_of_rows > 1:  # Should never happen
         raise Exception('More than one row of settlement data matched')
-    return data_for_date.iloc[0]
+    return data_for_previous_trading_day.iloc[0]
 
 
-def process_overnight_settlement_changes(settlement_csv_filenames: List[str], intraday_open_df: pd.DataFrame):
+def get_settlement_data_offset_by_interval(
+    a_date: datetime.date,
+    settlement_data_df: pd.DataFrame,
+    offset_interval: Settlement_Comparison_Interval,
+    unique_trading_days: List[datetime.date]
+):
+    match offset_interval.name:
+        case Settlement_Comparison_Interval.OVERNIGHT.name:
+            return get_settlement_data_for_previous_trading_day(
+                a_date=a_date,
+                settlement_data_df=settlement_data_df,
+                unique_trading_days=unique_trading_days
+            )
+        case _:
+            raise Exception('Unsupported Settlement_Comparison_Interval type')
+
+
+def process_overnight_settlement_changes(
+    settlement_csv_filenames: List[str],
+    intraday_open_df: pd.DataFrame,
+    settlement_comparison_report_interval: Settlement_Comparison_Interval,
+    unique_trading_days: List[datetime.date]
+):
     overnight_settlement_price_changes_df = pd.DataFrame(
         columns=['Date', 'Symbol', 'Price Difference b/w Open And Prior Day Settlement'])
     # Process each table of settlement data - one for each contract
@@ -115,16 +170,33 @@ def process_overnight_settlement_changes(settlement_csv_filenames: List[str], in
         for a_date in a_contracts_trading_dates:
             first_available_bar = get_first_bar_available_for_day(
                 a_date=a_date, a_contracts_open_data=a_contracts_open_data)
-            settlement_bar = get_settlement_data_for_date(
-                a_date=a_date, settlement_data_df=settlement_data_df)
-            difference_between_open_price_and_prior_day_settlement = calculate_overnight_change_from_settlement(
+            settlement_bar = get_settlement_data_offset_by_interval(
+                a_date=a_date,
+                settlement_data_df=settlement_data_df,
+                offset_interval=settlement_comparison_report_interval,
+                unique_trading_days=unique_trading_days
+            )
+            difference_between_open_price_and_prior_settlement = calculate_change_between_open_and_prior_settlement(
                 a_days_open_bar=first_available_bar, a_prior_days_settlement_bar=settlement_bar)
             overnight_settlement_price_changes_df = overnight_settlement_price_changes_df.append(
-                {'Symbol': 'LE' + contract_month_and_year, 'Date': a_date, 'Price Difference b/w Open And Prior Day Settlement': difference_between_open_price_and_prior_day_settlement}, ignore_index=True)
+                {'Symbol': 'LE' + contract_month_and_year, 'Date': a_date, 'Price Difference b/w Open And Prior Day Settlement': difference_between_open_price_and_prior_settlement}, ignore_index=True)
     return overnight_settlement_price_changes_df
 
 
+# Parse our arguments from the command line to determine the strategy
+try:
+    opts, args = getopt.getopt(sys.argv[1:], ':i:')
+except getopt.GetoptError:
+    print('Run this script as follows: create_price_changes_for_settlements.py.py -i <settlement_comparison_report_interval>')
+    sys.exit(2)
+for opt, arg in opts:
+    if opt in ('-i'):
+        settlement_comparison_report_interval = Settlement_Comparison_Interval(
+            arg)
+
 settlement_csv_filenames = settlement_csv_files_to_analyze(RAW_DATA_DIR)
+unique_trading_days = convert_unique_trading_days(
+    UNIQUE_TRADING_DAYS_LE_CONTRACTS_FILE_PATH)
 logging.info(f"Parsing intraday true open into dataframe")
 intraday_true_open_df = intraday_open_to_df(
     LIVE_CATTLE_INTRADAY_TRUE_OPEN_FILE_PATH)
@@ -132,9 +204,17 @@ logging.info(f"Parsing intraday sliding open into dataframe")
 intraday_sliding_open_df = intraday_open_to_df(
     LIVE_CATTLE_INTRADAY_SLIDING_OPEN_FILE_PATH)
 overnight_changes_true_open_df = process_overnight_settlement_changes(
-    settlement_csv_filenames=settlement_csv_filenames, intraday_open_df=intraday_true_open_df)
+    settlement_csv_filenames=settlement_csv_filenames,
+    intraday_open_df=intraday_true_open_df,
+    settlement_comparison_report_interval=settlement_comparison_report_interval,
+    unique_trading_days=unique_trading_days
+)
 overnight_changes_sliding_open_df = process_overnight_settlement_changes(
-    settlement_csv_filenames=settlement_csv_filenames, intraday_open_df=intraday_sliding_open_df)
+    settlement_csv_filenames=settlement_csv_filenames,
+    intraday_open_df=intraday_sliding_open_df,
+    settlement_comparison_report_interval=settlement_comparison_report_interval,
+    unique_trading_days=unique_trading_days
+)
 logging.info(f"Saving true open target csv to {TRUE_OPEN_TARGET_FILEPATH}")
 overnight_changes_true_open_df.to_csv(TRUE_OPEN_TARGET_FILEPATH, index=False)
 logging.info(
