0c1ce009aeca5379b4b580a51bbbce96ca4bdf83
Sam Cooper <cooper437@users.noreply.github.com>
2022-01-18 11:09:08 -0500

    Settlement add annual analysis (#32)
    * Added annual reporting for settlements data and added some comments and minor refactoring to the settlements changes etl script

--- etl/python_scripts/create_price_changes_for_settlements.py
+++ etl/python_scripts/create_price_changes_for_settlements.py
@@ -5,12 +5,19 @@ import pandas as pd
 import datetime
 from typing import List, Tuple
 from decimal import Decimal, ROUND_HALF_UP
-from pandas.core.frame import DataFrame
-from pandas.core.series import Series
 from tqdm import trange
 import logging
 import sys
 import getopt
+
+'''
+Analyze our futures contract settlement data and compare it with our intraday open contract data for both true open and sliding open.
+Use this analysis to build out a new set of tables that show the changes between the open price of each day and the settlement in the past.
+The settlement price for comparison depends on whether you run the script using an overnight, weekly, monthly, or annualy comparison interval.
+
+To change the Settlement_Comparison_Interval that the script uses you can pass in an argument as follows:
+create_price_changes_for_settlements.py -i overnight
+'''
 
 CURRENT_DIR = os.path.dirname(__file__)
 RAW_DATA_DIR = os.path.join(
@@ -37,10 +44,10 @@ logging.basicConfig(
 class Settlement_Comparison_Interval(enum.Enum):
     '''
     Represents the interval of time between the open price and the settlement price we are comparing it with
-    Overnight - Compare a days open price with the prior days settlement price
+    Overnight - Compare a days open price with the prior trading days settlement price
     Weekly - Compare a days open price with a settlement price from 7 days ago.
-    Monthly - Compare a days open price with a settlement price from 4 weeks ago
-    Annualy - Compare a days open price with a settlement price from a year ago
+    Monthly - Compare a days open price with a settlement price from 30 days ago
+    Annualy - Compare a days open price with the average settlement price of the same month one year ago
     '''
     OVERNIGHT = 'overnight'
     WEEKLY = 'weekly'
@@ -114,6 +121,9 @@ def get_settlement_data_for_previous_trading_day(
     settlement_data_df: pd.DataFrame,
     unique_trading_days: List[datetime.date]
 ) -> pd.Series:
+    '''
+    Get the settlement data for the previous trading day. If it's not available then return None.
+    '''
     index_of_a_date = unique_trading_days.index(a_date)
     try:
         prior_trading_day_date = unique_trading_days[index_of_a_date - 1]
@@ -123,7 +133,7 @@ def get_settlement_data_for_previous_trading_day(
                                                        prior_trading_day_date]
     number_of_rows = len(data_for_previous_trading_day.index)
     if number_of_rows == 0:
-        return (None, 0)
+        return (None, None)
     if number_of_rows > 1:  # Should never happen
         raise Exception('More than one row of settlement data matched')
     num_lookback_days = (a_date - prior_trading_day_date).days
@@ -132,24 +142,31 @@ def get_settlement_data_for_previous_trading_day(
     return (data_for_previous_trading_day.iloc[0], num_lookback_days)
 
 
-def get_settlement_data_for_day_in_previous_week(
+def get_settlement_data_for_lookback_days(
     a_date: datetime.date,
     settlement_data_df: pd.DataFrame,
     base_lookback_days: datetime.timedelta
 ) -> pd.Series:
+    '''
+    Get the settlement data for a day that is at least base_lookback_days in the past. If that's not available then
+    we go backwards in time in 1 day increments until either A) we find a day where we have settlement data available,
+    in which case we return it for that day or B) we are before the first_trading_day_available in the settlement dataset in which
+    case we return None.
+    '''
     unique_trading_days = np.sort(settlement_data_df['Date'].dt.date.values)
     first_trading_day_available = unique_trading_days[0]
-    additional_days_lookback = datetime.timedelta(days=1)
     lookback_date = a_date - base_lookback_days
     # Check if the date is a trading day and if so get the index
     if lookback_date in unique_trading_days:
         prior_trading_day_date = lookback_date
     # The date is not a trading day so we loop continuously backwards in time in 1 day incremenets to find the closest trading day before it
     else:
+        # Initialize to one additional day in the past
+        additional_days_lookback = datetime.timedelta(days=1)
         while (lookback_date not in unique_trading_days):
             lookback_date = lookback_date - additional_days_lookback
             if lookback_date < first_trading_day_available:  # A check to make sure we dont enter an infinite loop
-                return (None, 0)
+                return (None, None)
             # We found the closest trading day prior to a week ago
             if (lookback_date in unique_trading_days):
                 prior_trading_day_date = lookback_date
@@ -164,11 +181,36 @@ def get_settlement_data_for_day_in_previous_week(
     return (data_for_previous_trading_day.iloc[0], num_lookback_days)
 
 
+def get_settlement_data_for_avg_month(
+    a_date: datetime.date,
+    settlements_by_month_df: pd.DataFrame
+) -> pd.Series:
+    '''
+    Get the settlement data for the same month of the prior year if available. Note that value for this settlement price
+    has already been averaged (mean) in an earlier step in the script which is unlike the other report intervals.
+    '''
+    month = a_date.month
+    previous_year = a_date.year - 1
+    settlement_for_same_month_last_year_df = settlements_by_month_df[(
+        settlements_by_month_df['Year'] == previous_year) & (settlements_by_month_df['Month'] == month)]
+    number_of_rows = len(settlement_for_same_month_last_year_df.index)
+    if number_of_rows == 0:
+        return (None, None)
+    if number_of_rows > 1:  # Should never happen
+        raise Exception('More than one row of settlement data matched')
+    settlement_for_same_month_last_year_series = settlement_for_same_month_last_year_df.iloc[0]
+    logging.debug(
+        f"a_date={a_date} month={month} previous_year={previous_year}"
+    )
+    return (settlement_for_same_month_last_year_series, 365)
+
+
 def get_settlement_data_offset_by_interval(
     a_date: datetime.date,
     settlement_data_df: pd.DataFrame,
     offset_interval: Settlement_Comparison_Interval,
-    unique_trading_days: List[datetime.date]
+    unique_trading_days: List[datetime.date],
+    settlements_by_month_df: pd.DataFrame
 ):
     match offset_interval.name:
         case Settlement_Comparison_Interval.OVERNIGHT.name:
@@ -178,27 +220,50 @@ def get_settlement_data_offset_by_interval(
                 unique_trading_days=unique_trading_days
             )
         case Settlement_Comparison_Interval.WEEKLY.name:
-            return get_settlement_data_for_day_in_previous_week(
+            return get_settlement_data_for_lookback_days(
                 a_date=a_date,
                 settlement_data_df=settlement_data_df,
                 base_lookback_days=datetime.timedelta(days=7)
             )
         case Settlement_Comparison_Interval.MONTHLY.name:
-            return get_settlement_data_for_day_in_previous_week(
+            return get_settlement_data_for_lookback_days(
                 a_date=a_date,
                 settlement_data_df=settlement_data_df,
                 base_lookback_days=datetime.timedelta(days=30)
+            )
+        case Settlement_Comparison_Interval.ANNUALY.name:
+            return get_settlement_data_for_avg_month(
+                a_date=a_date,
+                settlements_by_month_df=settlements_by_month_df
             )
         case _:
             raise Exception('Unsupported Settlement_Comparison_Interval type')
 
 
-def process_overnight_settlement_changes(
+def group_settlement_data_by_month(settlement_data_df: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Group the settlement data by month and take the mean value for each month
+    '''
+    settlement_data_limited_df = settlement_data_df[['Date', 'Settle']].copy()
+    settlement_data_limited_df = settlement_data_limited_df.set_index('Date')
+    settlement_mean_by_month_df = settlement_data_limited_df.resample(
+        'M').mean()
+    settlement_mean_by_month_df['Year'] = settlement_mean_by_month_df.index.year
+    settlement_mean_by_month_df['Month'] = settlement_mean_by_month_df.index.month
+    return settlement_mean_by_month_df
+
+
+def process_settlement_changes(
     settlement_csv_filenames: List[str],
     intraday_open_df: pd.DataFrame,
     settlement_comparison_report_interval: Settlement_Comparison_Interval,
     unique_trading_days: List[datetime.date]
-):
+) -> pd.DataFrame:
+    '''
+    Process each contract and then each day within each contract calculcating the difference between the open price for that day
+    and the settlement price at some point in the past (day, week, month, year). Return a dataframe that containes the price difference
+    for that day.
+    '''
     overnight_settlement_price_changes_df = pd.DataFrame(
         columns=['Date', 'Symbol', 'Price Difference b/w Open And Prior Day Settlement'])
     # Process each table of settlement data - one for each contract
@@ -207,6 +272,8 @@ def process_overnight_settlement_changes(
         contract_month_and_year = contract_month_and_year_from_file_name(
             filename)
         settlement_data_df = settlement_data_to_df(filename)
+        settlements_by_month_df = group_settlement_data_by_month(
+            settlement_data_df)
         a_contracts_open_data = intraday_open_df[intraday_open_df['Symbol'].str[-3:]
                                                  == contract_month_and_year]
         a_contracts_trading_dates = a_contracts_open_data['DateTime'].dt.date\
@@ -219,7 +286,8 @@ def process_overnight_settlement_changes(
                 a_date=a_date,
                 settlement_data_df=settlement_data_df,
                 offset_interval=settlement_comparison_report_interval,
-                unique_trading_days=unique_trading_days
+                unique_trading_days=unique_trading_days,
+                settlements_by_month_df=settlements_by_month_df
             )
             difference_between_open_price_and_prior_settlement = calculate_change_between_open_and_prior_settlement(
                 a_days_open_bar=first_available_bar, a_prior_days_settlement_bar=settlement_bar)
@@ -233,11 +301,11 @@ def process_overnight_settlement_changes(
     return overnight_settlement_price_changes_df
 
 
-# Parse our arguments from the command line to determine the strategy
+# Parse our arguments from the command line to determine the settlement comparison report interval
 try:
     opts, args = getopt.getopt(sys.argv[1:], ':i:')
 except getopt.GetoptError:
-    print('Run this script as follows: create_price_changes_for_settlements.py.py -i <settlement_comparison_report_interval>')
+    print('Run this script as follows: create_price_changes_for_settlements.py -i <settlement_comparison_report_interval>')
     sys.exit(2)
 for opt, arg in opts:
     if opt in ('-i'):
@@ -254,13 +322,13 @@ logging.info(f"Parsing intraday sliding open into dataframe")
 intraday_sliding_open_df = intraday_open_to_df(
     LIVE_CATTLE_INTRADAY_SLIDING_OPEN_FILE_PATH)
 # Overnight Interval
-overnight_changes_true_open_df = process_overnight_settlement_changes(
+overnight_changes_true_open_df = process_settlement_changes(
     settlement_csv_filenames=settlement_csv_filenames,
     intraday_open_df=intraday_true_open_df,
     settlement_comparison_report_interval=settlement_comparison_report_interval,
     unique_trading_days=unique_trading_days
 )
-overnight_changes_sliding_open_df = process_overnight_settlement_changes(
+overnight_changes_sliding_open_df = process_settlement_changes(
     settlement_csv_filenames=settlement_csv_filenames,
     intraday_open_df=intraday_sliding_open_df,
     settlement_comparison_report_interval=settlement_comparison_report_interval,
