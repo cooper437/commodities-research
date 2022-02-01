27eb7ea11e2737f9f6a2ffa47d96168b57980c59
Sam Cooper <cooper437@gmail.com>
2022-02-01 12:51:43 -0500

    WIP

--- etl/python_scripts/create_settlement_volatility_datasets.py
+++ etl/python_scripts/create_settlement_volatility_datasets.py
@@ -51,12 +51,37 @@ def contract_month_and_year_from_file_name(filename: str) -> str:
     return settlement_data_contract_month + settlement_data_contract_year[-2:]
 
 
-def calc_custom_std_deviation(a_price_series: pd.Series) -> int:
+def calc_custom_std_deviation(a_price_series: pd.Series) -> Decimal:
     distance_to_zero_squared_series = a_price_series.apply(
         lambda x: pow(abs(x), 2))
     mean_distance_to_zero_squared_series = distance_to_zero_squared_series.mean()
     square_root = math.sqrt(mean_distance_to_zero_squared_series)
     return round_to_nearest_thousandth(square_root)
+
+
+def calc_range_for_series(a_price_series: pd.Series) -> Decimal:
+    print(a_price_series)
+    a_max = a_price_series.max()
+    a_min = a_price_series.min()
+    a_range = round_to_nearest_thousandth(a_max - a_min)
+    return a_range
+
+
+def calc_thirty_day_stats(
+    within_last_thirty_days_df: pd.DataFrame,
+    a_date: datetime.date,
+    first_date_for_contract: datetime.date
+) -> Tuple:
+    # Skip calculating the 30 day stats for the first thirty days of settlement data for every contract
+    if (a_date - first_date_for_contract).days <= 30:
+        csd_thirty = None
+        thirty_day_range = None
+    else:
+        csd_thirty = calc_custom_std_deviation(
+            within_last_thirty_days_df['Settle'])
+        thirty_day_range = calc_range_for_series(
+            within_last_thirty_days_df['Settle'])
+    return (thirty_day_range, csd_thirty)
 
 
 def process_settlement_volatility(
@@ -77,18 +102,32 @@ def process_settlement_volatility(
                 'Date': a_date,
                 'Symbol': 'LE' + contract_month_and_year
             }
+            a_date_year_prior = a_date - datetime.timedelta(days=365)
             a_date_30_days_prior = a_date - datetime.timedelta(days=30)
-            within_last_thirty_days_df = settlement_data_df[(settlement_data_df['Date'].dt.date >= a_date_30_days_prior) & (
+            a_date_7_days_prior = a_date - datetime.timedelta(days=7)
+            within_last_year_df = settlement_data_df[(settlement_data_df['Date'].dt.date >= a_date_year_prior) & (
                 settlement_data_df['Date'].dt.date <= a_date)]
+            within_last_thirty_days_df = within_last_year_df[(within_last_year_df['Date'].dt.date >= a_date_30_days_prior) & (
+                within_last_year_df['Date'].dt.date <= a_date)]
+            within_last_seven_days_df = within_last_thirty_days_df[(within_last_thirty_days_df['Date'].dt.date >= a_date_7_days_prior) & (
+                within_last_thirty_days_df['Date'].dt.date <= a_date)]
             a_date_metadata['30D Count'] = len(
                 within_last_thirty_days_df.index)
-            # Skip calculting the 30 day CSD for the first thirty days of settlement data for every contract
-            if (a_date - first_date_for_contract).days <= 30:
-                a_date_metadata['30D CSD'] = None
-            else:
-                csd_thirty = calc_custom_std_deviation(
-                    within_last_thirty_days_df['Settle'])
-                a_date_metadata['30D CSD'] = csd_thirty
+            a_date_metadata['7D Count'] = len(
+                within_last_seven_days_df.index)
+            a_date_metadata['365D Count'] = len(
+                within_last_year_df.index)
+            a_date_metadata['365D Range'] = calc_range_for_series(
+                within_last_year_df['Settle'])
+            a_date_metadata['7D Range'] = calc_range_for_series(
+                within_last_seven_days_df['Settle'])
+            (thirty_day_range, csd_thirty) = calc_thirty_day_stats(
+                within_last_thirty_days_df=within_last_thirty_days_df,
+                a_date=a_date,
+                first_date_for_contract=first_date_for_contract
+            )
+            a_date_metadata['30D CSD'] = csd_thirty
+            a_date_metadata['30D Range'] = thirty_day_range
             all_dates_metadata.append(a_date_metadata)
     print('hello')
 
